a33363f951f30a0c276a56116423ba252983377e
Greg <you@example.com>
2016-11-09 22:59:42 -0500

    Added comments and some fixes to dictionary and bookshelf mlis

--- Dictionary.mli
+++ Dictionary.mli
@@ -1,5 +1,13 @@
-type single_definition = {typ : string; sub_definition : string}
+(* Controls interactions with the WordNet Dictionary API *)
 
-type all_definitions = single_definition list
+module Dictionary = sig
+	(* exception - no definition was found for the given string *)
+	exception Word_Not_Found
+	
+	(* The type of a definition *)
+  type definition = string
 
-val get_definition : string -> all_definitions
+	(* Returns the definition of a given word, or raises Word_Not_Found *)
+	(* exception if the word was not found *)
+	val get_definition : string -> definition
+end
--- bookshelf.mli
+++ bookshelf.mli
@@ -1,18 +1,31 @@
-type bookshelf_id = int
+(* Controls and stores data about available books *)
 
-type book_id = int
-
-type book_list = string list
-
-type book_data = {id : book_id; title : string; author : string; 
-    num_pages : int; current_position : int}
-
-val list_books : bookshelf_id -> book_list
-
-val open_book : book_id -> string
-
-val close_book : book_id -> boolean
-
-val get_num_books : bookshelf_id -> int
-
-val get_book_data : book_id -> book_data
+module Bookshelf = sig
+	(* The unique identifier for a bookshelf *)
+  type bookshelf_id = int
+  
+	(* The unique identifier for a book *)
+  type book_id = int
+  
+	(* The string of text that represents the content of a book *)
+  type book_text = string
+  
+	(* The data contained by an individual book *)
+  type book_data = {id : book_id; title : string; author : string; 
+      current_position : int}
+  
+	(* Lists the books currently on the bookshelf with the given ID *)
+  val list_books : bookshelf_id -> book_list
+  
+	(* Returns the text of a book *)
+  val get_book : book_id -> book_text
+  
+	(* Closes a book and saves current reading position *)
+  val close_book : book_id -> bool
+  
+	(* Returns the number of books in the given bookshelf *)
+  val get_num_books : bookshelf_id -> int
+  
+	(* Returns the data for a given book *)
+  val get_book_data : book_id -> book_data
+end
