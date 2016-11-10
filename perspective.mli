0cc39817720dbeb9e9904b89923156aed9265533
Arnav Ghosh <garnav@gmail.com>
2016-11-09 23:22:59 -0500

    fixed perspective

--- perspective.mli
+++ perspective.mli
@@ -1,16 +1,24 @@
 module type Perspective = sig
   open QA
+	open Marginalia
   
   (* [t] represents a type of multiple pages. *) 
-  type t 
+  type t
+	
+	type page = Marginalia.t
 
   (* [update_page_range it1 i] is [t] with the page represented by
   [it1] present along with i consecutive pages.*)
   val update_page_range : int * int -> int -> t
 
-  val page_annotations : t -> [> H of ]
+  (* [page_annotations t1] is [page], the annotations for
+	a given page, as bounded by a starting and ending index. *)
+  val page_annotations : t -> page
 
-  val skim : t -> 
+  (* [skim it1 t1] is a list of notes that are relevant to the notes
+	associated with the page bounded by it1. [it1] is (s,t)
+	where s is the starting index and t is the ending index. *)
+  val skim : int * int -> t -> string list
 
   (* [flash_card str] is a list of strings, present as annotations,
   relevant to the given term [str] and the indices at which these strings
