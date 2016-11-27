a1543cafffbdcaa5e449a5432a82f7e207e22129
RaghavBatra <tb698@cornell.edu>
2016-11-27 17:44:44 -0500

    modified DC and UI

--- data_controller.mli
+++ data_controller.mli
@@ -1,23 +1,20 @@
 module DataController = sig
 
   (* the type of the data structure that stores relevant information
-  about the book such as number of pages, fontsize, current page, etc. *)
+  about the book such as the book name, number of pages, current page, book text, font size, etc. *)
   type t 
-  
-  (* Allows the user to increase or decrease the fontsize *)
-  val change_fontsize: t -> t
 
-  (* Allows the user to change the font *)
-  val change_font: t -> t
+  (* Opens the current file *)
+  val open_file: string -> unit
+
+  (* Closes the current file *)
+  val close_file: t -> unit
+
+  (* Change font color *)
+  val change_font_color: ANSITerminal.color -> t -> unit
 
   (* Returns the current page being read *)
   val curr_page: t -> int
-
-  (* Opens the current file *)
-  val open_file: string -> t
-
-  (* Closes the current file *)
-  val close_file: string -> t 
 
   (* finds the meaning of the given word *)
   val find_meaning: string -> string
@@ -25,10 +22,28 @@ module DataController = sig
   (* Outputs the percentage of the book read *)
   val percent_read: t -> float
 
-  (* Allows the user to bookmark this page *)
-  val bookmark: t -> t
+  (* Turn to next page *)
+  val next_page: t -> t
+  
+  (* Turn to previous page *)
+  val prev_page: t -> t
 
-  (* Allows the user to make notes relating to the current page *)
-  val make_note: t -> t
+  (* Allows the user to add a bookmark to this page *)
+  val add_bookmark: t -> t
+  
+  (* Allows the user to delete a bookmark from this page *)
+  val delete_bookmark: t -> t
+
+  (* Allows the user to add notes regarding this page *)
+  val add_notes: t -> t
+  
+  (* Allows the user to delete notes regarding this page *)
+  val delete_notes: t -> t
+  
+  (* Allows the user to add highlights regarding this page *)
+  val add_highlights: t -> t
+  
+  (* Allows the user to delete notes regarding this page *)
+  val delete_highlights: t -> t
  
 end
