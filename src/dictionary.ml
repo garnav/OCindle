25ba5df86b2981188da5162937fe95f3ebf0a55c
Greg <you@example.com>
2016-12-03 15:41:03 -0500

    added dictionary.ml and a stub for get_definition

---
+++ src/dictionary.ml
@@ -0,0 +1,16 @@
+(* open Wordnet *) (* Will uncomment later *)
+
+module Dictionary = struct
+
+  (* exception - no definition was found for the given string *)
+  exception Word_Not_Found
+  
+  type definition = string
+
+  (* [get_definition word] Returns the definition of [word],
+  or raises Word_Not_Found exception if the word was not found *)
+  let get_definition word =
+    "definition placeholder"
+     (* Wordnet.getWord "chair" Wordnet.NOUN *)
+    
+end
